85ff1f9ec1d30f90777ad1e0fdc27156cfc7a2ed
Michael Caselli <michael.caselli@gmail.com>
2025-09-22 13:38:20 -0400

    shorten the column names returned by accounts_by_status() (dropping the _accounts suffix everywhere). store accounts as character vectors, not lists. add some unit tests for accounts_by_status()

--- R/accounts_by_status.R
+++ R/accounts_by_status.R
@@ -41,30 +41,30 @@ active_accounts_in_range <- function(account_id, order_date, start_date, end_dat
 #' The function returns a data frame with the following columns:
 #' - `period_start` (date): The start date of the period.
 #' - `period_end` (date) : The end date of the period.
-#' - `active_accounts` (list of character): Accounts with orders in the current
+#' - `active` (list of character): Accounts with orders in the current
 #' period.
-#' - `new_accounts` (list of character): Accounts with their first-ever order in
+#' - `new` (list of character): Accounts with their first-ever order in
 #' the current period.
-#' - `returning_accounts` (list of character): Accounts with orders in both the
+#' - `returning` (list of character): Accounts with orders in both the
 #' current and prior periods.
-#' - `temporarily_lost_accounts` (list of character): Accounts with orders in
+#' - `temporarily_lost` (list of character): Accounts with orders in
 #' the prior period but not in the current period, yet have orders in future
 #' periods.
-#' - `terminally_lost_accounts` (list of character): Accounts with orders in the
+#' - `terminally_lost` (list of character): Accounts with orders in the
 #' prior period but not in the current period, and no orders in future periods.
-#' - `regained_accounts` (list of character): Accounts with orders in the current period, no orders in
+#' - `regained` (list of character): Accounts with orders in the current period, no orders in
 #' the prior period, but had orders in earlier periods. i.e. an account that
 #' comes back after being "temporarily lost"
 #'
 #' if `with_counts` is TRUE, additional columns are included:
-#' - `n_active_accounts` (int): Count of active accounts in the current period.
-#' - `n_new_accounts` (int): Count of new accounts in the current period.
-#' - `n_returning_accounts` (int): Count of returning accounts in the current period.
-#' - `n_temporarily_lost_accounts` (int): Count of temporarily lost accounts in the
+#' - `n_active` (int): Count of active accounts in the current period.
+#' - `n_new` (int): Count of new accounts in the current period.
+#' - `n_returning` (int): Count of returning accounts in the current period.
+#' - `n_temporarily_lost` (int): Count of temporarily lost accounts in the
 #' current period.
-#' - `n_terminally_lost_accounts` (int): Count of terminally lost accounts in the
+#' - `n_terminally_lost` (int): Count of terminally lost accounts in the
 #' current period.
-#' - `n_regained_accounts` (int): Count of regained accounts in the current period.
+#' - `n_regained` (int): Count of regained accounts in the current period.
 #' @export
 #'
 #' @examples
@@ -96,7 +96,7 @@ accounts_by_status <- function(account_id, order_date, by = c("month", "quarter"
     period_end = period_end
   ) |>
     mutate(
-      active_accounts = purrr::map2(
+      active = purrr::map2(
         period_start, period_end,
         ~ active_accounts_in_range(
           account_id = df$account_id,
@@ -108,56 +108,56 @@ accounts_by_status <- function(account_id, order_date, by = c("month", "quarter"
     )
 
   for (i in 1:nrow(result)) {
-    cumulative_prior_accounts <- if (i > 1) {
-      unique(unlist(result$active_accounts[1:i - 1]))
+    cumulative_prior <- if (i > 1) {
+      unique(unlist(result$active[1:i - 1]))
     } else {
       character(0)
     }
-    prior_period_active_accounts <- if (i > 1) {
-      unlist(result$active_accounts[[i - 1]])
+    prior_period_active <- if (i > 1) {
+      unlist(result$active[[i - 1]])
     } else {
       character(0)
     }
-    cumulative_future_active_accounts <- if (i < nrow(result)) {
-      unique(unlist(result$active_accounts[(i + 1):nrow(result)]))
+    cumulative_future_active <- if (i < nrow(result)) {
+      unique(unlist(result$active[(i + 1):nrow(result)]))
       # } else character(0) # assume no returning customers after the final period
     } else {
       # assume all customers ordering in the final period eventually come back
-      result$active_accounts[[nrow(result)]]
+      result$active[[nrow(result)]]
     }
 
-    # result$cumulative_prior_accounts[[i]] <- list(cumulative_prior_accounts)
-    new_accounts <- setdiff(result$active_accounts[[i]], cumulative_prior_accounts)
-    result$new_accounts[[i]] <- list(new_accounts)
+    # result$cumulative_prior[[i]] <- list(cumulative_prior)
+    new <- setdiff(result$active[[i]], cumulative_prior)
+    result$new[[i]] <- new
 
-    returning_accounts <- intersect(result$active_accounts[[i]], prior_period_active_accounts)
-    result$returning_accounts[[i]] <- list(returning_accounts)
+    returning <- intersect(result$active[[i]], prior_period_active)
+    result$returning[[i]] <- returning
 
-    terminally_lost_accounts <- setdiff(
-      setdiff(prior_period_active_accounts, result$active_accounts[[i]]),
-      cumulative_future_active_accounts
+    terminally_lost <- setdiff(
+      setdiff(prior_period_active, result$active[[i]]),
+      cumulative_future_active
     )
-    result$terminally_lost_accounts[[i]] <- list(terminally_lost_accounts)
+    result$terminally_lost[[i]] <- terminally_lost
 
-    temporarily_lost_accounts <- intersect(
-      setdiff(prior_period_active_accounts, result$active_accounts[[i]]),
-      cumulative_future_active_accounts
+    temporarily_lost <- intersect(
+      setdiff(prior_period_active, result$active[[i]]),
+      cumulative_future_active
     )
-    result$temporarily_lost_accounts[[i]] <- list(temporarily_lost_accounts)
+    result$temporarily_lost[[i]] <- temporarily_lost
 
-    regained_accounts <- setdiff(setdiff(result$active_accounts[[i]], prior_period_active_accounts), new_accounts)
-    result$regained_accounts[[i]] <- list(regained_accounts)
+    regained <- setdiff(setdiff(result$active[[i]], prior_period_active), new)
+    result$regained[[i]] <- regained
   }
 
   result <- result |>
-    dplyr::relocate(regained_accounts, temporarily_lost_accounts, terminally_lost_accounts, .after = returning_accounts)
+    dplyr::relocate(regained, temporarily_lost, terminally_lost, .after = returning)
 
   if (with_counts){
     result <- result |>
       dplyr::rowwise() |>
       dplyr::mutate(
         across(
-          .cols = dplyr::ends_with("_accounts"),
+          .cols = -c(period_start, period_end),
           .fns = ~ length(unlist(.x)),
           .names = "n_{.col}"
         )
--- tests/testthat/test-accounts_by_status.R
+++ tests/testthat/test-accounts_by_status.R
@@ -89,4 +89,62 @@ test_that("active_accounts_in_range() works with factors", {
 })
 
 
+test_that("accounts_by_status() properly calculates period_start and period_end",
+{
+  orders <- data.frame(
+    account_id = c("A", "B", "C"),
+    order_date = as.Date(c(
+      "2022-09-15", "2023-01-10", "2022-10-05"
+    ))
+  )
+
+  result_monthly <- accounts_by_status(orders$account_id, orders$order_date, by = "month")
+  expect_equal(result_monthly$period_start, as.Date(c("2022-09-01", "2022-10-01", "2022-11-01", "2022-12-01", "2023-01-01")))
+  expect_equal(result_monthly$period_end, as.Date(c("2022-09-30", "2022-10-31", "2022-11-30", "2022-12-31", "2023-01-31")))
+
+  result_quarterly <- accounts_by_status(orders$account_id, orders$order_date, by = "quarter")
+  expect_equal(result_quarterly$period_start, as.Date(c("2022-07-01", "2022-10-01", "2023-01-01")))
+  expect_equal(result_quarterly$period_end, as.Date(c("2022-09-30", "2022-12-31", "2023-03-31")))
+
+})
+
+
+test_that("accounts_by_status() correctly lists new accounts", {
+
+  orders <- dplyr::tribble(
+    ~account_id, ~order_date,
+    "A", "2022-01-15",
+    "B", "2022-01-20",
+    "A", "2022-02-10",
+    "C", "2022-02-15",
+    "D", "2022-03-05",
+    "E", "2022-03-25",
+    "B", "2022-04-10",
+    "F", "2022-04-15"
+  ) |> dplyr::mutate(order_date = as.Date(order_date))
+
+  result <- accounts_by_status(orders$account_id, orders$order_date)
+
+  expected <- dplyr::tribble(
+    ~period_start, ~new,
+    as.Date("2022-01-01"), c("A", "B"),
+    as.Date("2022-02-01"), c("C"),
+    as.Date("2022-03-01"), c("D", "E"),
+    as.Date("2022-04-01"), c("F")
+  ) |> data.frame()
+
+  expect_equal(result |> dplyr::select(period_start, new), expected)
+
+  expected_quarterly <- dplyr::tribble(
+    ~period_start, ~new,
+    as.Date("2022-01-01"), c("A", "B", "C", "D", "E"),
+    as.Date("2022-04-01"), c("F")
+  ) |> data.frame()
+
+  result_quarterly <- accounts_by_status(orders$account_id, orders$order_date, by = "quarter")
+
+  expect_equal(result_quarterly |> dplyr::select(period_start, new), expected_quarterly)
+
+})
+
 
