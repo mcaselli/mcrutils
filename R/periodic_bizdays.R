44647e0d178c5f718d587e619f9f5050de38b7ce
Michael Caselli <michael.caselli@gmail.com>
2025-10-16 13:36:29 -0400

    fix for periodic_bizdays() when from and/or to are in the middle of a calendar period (e.g. mid-month, mid-quarter)

--- R/periodic_bizdays.R
+++ R/periodic_bizdays.R
@@ -1,12 +1,12 @@
 #' Calculate the number of business days in periodic intervals
 #'
-#' This function calculates the number of business days in periodic intervals
-#' (e.g., monthly, quarterly) between two dates, using specified QuantLib
-#' calendars for holiday definitions. Convenience wrapper for
-#' [bizdays::bizdays()].
+#' A Convenience wrapper for [bizdays::bizdays()]. This function calculates the
+#' number of business days in periodic intervals (e.g., monthly, quarterly)
+#' between two dates, using specified QuantLib calendars for holiday
+#' definitions.
 #'
-#' @param from start date (Date)
-#' @param to end date (Date)
+#' @param from,to start and end dates. Date object or something coercible
+#' with [as.Date()].
 #' @param by periodicity, passed to [seq.Date()] (e.g., "month", "quarter",
 #'   "year"). See `?seq.Date` for more options.
 #' @param quantlib_calendars = a character vector of RQuantLib calendar names
@@ -15,9 +15,9 @@
 #' @return a tibble with columns:
 #' \describe{
 #'  \item{calendar}{the QuantLib calendar used}
-#'  \item{start}{the start date of the interval}
-#'  \item{end}{the end date of the interval}
-#'  \item{business_days}{the number of business days in the interval,
+#'  \item{start}{the start date of the period}
+#'  \item{end}{the end date of the period}
+#'  \item{business_days}{the number of business days in the period,
 #'    according to the calendar}
 #'  }
 #' @examples
@@ -30,6 +30,9 @@
 #' @export
 periodic_bizdays <- function(from, to, by = "month",
                              quantlib_calendars = "UnitedStates") {
+  from <- as.Date(from)
+  to <- as.Date(to)
+
   suppressMessages(
     bizdays::load_quantlib_calendars(quantlib_calendars,
       from = from,
@@ -38,17 +41,24 @@ periodic_bizdays <- function(from, to, by = "month",
     )
   )
 
+  start <- seq(from, to, by = by)
+  end <- dplyr::lead(start) - 1
+  end[length(end)] <- to
+
   tidyr::expand_grid(
     calendar = quantlib_calendars,
-    start = seq(from, to, by = by)
+    data.frame(start = start, end = end)
   ) |>
-    dplyr::rowwise() |>
     mutate(
-      end = lubridate::rollback(lubridate::ceiling_date(.data$start, unit = by)),
-      business_days = bizdays::bizdays(
-        .data$start,
-        .data$end,
-        paste0("QuantLib/", .data$calendar)
+      business_days = purrr::pmap_int(
+        list(.data$start, .data$end, .data$calendar),
+        \(start, end, calendar) {
+          bizdays::bizdays(
+            from = start,
+            to = end,
+            cal = paste0("QuantLib/", calendar)
+          )
+        }
       )
     )
 }
