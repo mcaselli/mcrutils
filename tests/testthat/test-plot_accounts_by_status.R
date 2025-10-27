757386e726a7b81f50e80c7ab1b649b709a45d4e
Michael Caselli <michael.caselli@gmail.com>
2025-10-27 12:31:44 -0400

    changes to example_sales dataset to include units ordered, market (county), and also better simulate working/non-working days. Vignette now shows calculation of working day of period lookup table, shows cumulative daily sales plots

--- tests/testthat/test-plot_accounts_by_status.R
+++ tests/testthat/test-plot_accounts_by_status.R
@@ -6,6 +6,7 @@ test_that("plot_accounts_by_status() with incomplete final period", {
   )
 })
 
+
 test_that("plot_accounts_by_status() can force a final period to display as complete", {
   vdiffr::expect_doppelganger(
     "force_complete_final",
@@ -21,12 +22,13 @@ test_that("plot_accounts_by_status() displays a complete final period as solid",
     example_sales |>
       rbind(data.frame(
         account_id = "f",
-        order_date = as.Date("2024-12-31")
+        market = "United States",
+        order_date = as.Date("2024-12-31"),
+        units_ordered = 4
       )) |>
       plot_accounts_by_status(account_id, order_date, by = "quarter")
   )
 })
-
 
 
 test_that("plot_accounts_by_status() can exclude cumulative", {
