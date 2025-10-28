3e8ee02642910a4834e1eebbb0ecfa7ee4d5b19a
Michael Caselli <michael.caselli@gmail.com>
2025-10-28 15:48:04 -0400

    better docs

--- R/adjust_to_bizday.R
+++ R/adjust_to_bizday.R
@@ -1,12 +1,16 @@
 #' Adjust any non-working days to the following business day in a given calendar
 #'
 #' @param date A vector of dates (Date object or coercible with [as.Date()]).
-#' @param calendar (character) A QuantLib calendar id (the vector [qlcal::calendars] lists all valid options).
-#' @return A vector of Date objects adjusted to the following business day in the specified calendar.
+#' @param calendar (character) A QuantLib calendar id (the vector
+#'   [qlcal::calendars] lists all valid options).
+#' @return A vector of Date objects, the same length as date, with any
+#'   non-working dates adjusted to the following business day in the specified
+#'   calendar. Working days are left unchanged.
 #' @seealso [qlcal::calendars()], [qlcal::adjust()]
 #' @examples
-#' adjust_to_bizday(c("2025-07-04", "2025-05-31"), "UnitedStates")
-#' adjust_to_bizday(c("2025-01-15", "2025-01-16"), "UnitedStates")
+#' # July 4 is a US holiday, but not a UK holiday
+#' adjust_to_bizday(c("2025-07-03", "2025-07-04"), "UnitedStates")
+#' adjust_to_bizday(c("2025-07-03", "2024-07-04"), "UnitedKingdom")
 #' @export
 adjust_to_bizday <- function(date, calendar) {
   date <- as.Date(date)
